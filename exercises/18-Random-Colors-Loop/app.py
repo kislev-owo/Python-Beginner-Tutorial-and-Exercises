9add650240abb3aa205f4ac0858ba0bf164a1edb
kislev-owo <kislev_083@hotmail.com>
2020-08-21 19:58:54 +0000

    ejercicio 18 resuelto

--- exercises/18-Random-Colors-Loop/app.py
+++ exercises/18-Random-Colors-Loop/app.py
@@ -14,16 +14,26 @@ def get_color(color_number=4):
     return switcher.get(color_number,"Invalid Color Number")
 
 
-def get_allStudentColors():
+def get_allStudentColors(): ## se crea la funcion de get_allstudentcolors()
 
-    example_color = 1
-    students_array = []
+    students_array = []     ## se crea el arreglo students_array donde se       
+                            ## guardara el arreglo de el ciclo
+
     #your loop here
-    for i in range(10):
-        random_color_number = random.randint(0,4)
-        color = get_color(random_color_number)
-        student_array.append(color)
+    for i in range(10):      ## para i hasta 10 hace un ciclo de 0 - 10
 
-    return students_array
+        random_color_number = random.randint(0,4)   ## aqui en  
+                                                    ##random_color_number se le agrega un numero del 0 - 4
+        
+        color = get_color(random_color_number)      ## el numero aleatorio que 
+                                                    ##salio se multiplica con get_color y agarra uno de los del switcher de arriba luego se guarda en color
 
-print(get_allStudentColors())
+        students_array.append(color)                ## el color que salio del 
+                                                    ##swicher agrega con el metodo append (agregar) al ultimo elemento de la lista
+
+    return students_array   ##     devuelve el resultado al arreglo
+
+print(get_allStudentColors())   ## imprime la funcion
+
+
+
